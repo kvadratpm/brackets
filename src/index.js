4caa2c4f8fb8463ffe26b3f50fc16f7dfbed4c0f
Maksim Perevyazkin <kvadratpm@mail.ru>
2020-10-09 11:18:52 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const open = [];
+    const close = []; 
+    bracketsConfig.map((elem) => {
+      open.push(elem[0]);
+      close.push(elem[1]);
+      });
+    const current = [];
+    const arr = str.split('');
+    for (let i = 0; i < arr.length; i++) {
+      if (open.includes(arr[i]) && close.includes(arr[i])) {
+        if (!current.includes(arr[i]) && open.includes(arr[i])) {
+          current.push(arr[i]);
+        } else {
+          const previous = current.pop();
+          if (previous !== arr[i]) {
+            return false;
+          }
+        }
+      } else if (open.includes(arr[i])) {
+        current.push(arr[i]);
+      } else if (close.includes(arr[i])) {
+          const previous = current.pop();
+          if (open.indexOf(previous) !== close.indexOf(arr[i])) {
+            return false;
+          }
+      }
+    };
+    return current.length === 0;
 }
